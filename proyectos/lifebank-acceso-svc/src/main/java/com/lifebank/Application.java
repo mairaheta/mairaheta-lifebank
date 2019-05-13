31ddec48502872908e940a07b62677adc8c5dd38
Marcela Iraheta <marcelairaheta@gmail.com>
2019-05-12 21:58:25 -0600

    Entrega Final

--- proyectos/lifebank-acceso-svc/src/main/java/com/lifebank/Application.java
+++ proyectos/lifebank-acceso-svc/src/main/java/com/lifebank/Application.java
@@ -5,12 +5,9 @@ import java.net.UnknownHostException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableDiscoveryClient
+
 @SpringBootApplication
-@EnableZuulProxy
 public class Application {
 
 	public static void main(String[] args) {
